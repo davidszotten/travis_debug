28e83c8471a8d005f31bb719fbc4e2f0a2ce3c9f
David Szotten <davidszotten@gmail.com>
2014-05-07 09:09:52 +0100

    show source

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from mock import MagicMock
+import inspect
+
+import mock
 
 
 def display(value):
@@ -6,7 +8,7 @@ def display(value):
     print repr(value)
     print
 
-dict_map = MagicMock()
+dict_map = mock.MagicMock()
 display(dict_map)
 
 value = dict_map.get('value')
@@ -16,3 +18,7 @@ display(value)
 print value < 0
 print value == 0
 print value > 0
+
+print "############################################################"
+print inspect.getsource(mock)
+print "############################################################"
