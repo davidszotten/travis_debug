98cdf01347aa933e810af0aa44d79e9cc007add0
David Szotten <davidszotten@gmail.com>
2014-05-08 09:56:37 +0100

    type ordering

--- test.py
+++ test.py
@@ -9,3 +9,23 @@ print type(m1) < type(0)
 print type(m1) is MagicMock
 print isinstance(m1, MagicMock)
 print MagicMock < int
+
+
+class Foo(object):
+    pass
+
+class foo(object):
+    pass
+
+class bar(object):
+    pass
+
+class Bar(object):
+    pass
+
+print "types"
+print "Foo defined before foo"
+print "Bar defined after bar"
+
+print "Foo < foo:", Foo < foo
+print "Bar < bar:", Bar < bar
