02dcf5ee294a1bd6c79d0947ea1ffe6b94d39522
David Szotten <davidszotten@gmail.com>
2014-05-08 09:31:57 +0100

    printign

--- test.py
+++ test.py
@@ -1,29 +1,15 @@
-from mock import Mock, MagicMock
+from mock import MagicMock
 
-m1 = Mock()
+m1 = MagicMock()
 
-m4 = MagicMock()
-m5 = MagicMock()
-m5.__lt__.return_value = NotImplemented
-m6 = MagicMock()
-m6.__lt__.side_effect = lambda x: NotImplemented
+def print_and_return(*args, **kwargs):
+    print args, kwargs
+    return NotImplemented
 
-
-class Foo(object):
-    def __lt__(self, other):
-        return NotImplemented
+m1.__lt__.side_effect = print_and_return
+m1.__gt__.side_effect = print_and_return
 
 
-foo = Foo()
+print m1 < 0
 
-m7 = Mock(wraps=foo)
-m8 = MagicMock(wraps=foo)
-
-print 1, m1 < 0
-print 4, m4 < 0
-print 5, m5 < 0
-print 6, m6 < 0
-print 7, m7 < 0
-print 8, m8 < 0
-
-print 'f', foo < 0
+print object.__getattribute__(m1, '__lt__')(0)
